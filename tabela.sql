5c3e1dfdca208985c792711d0d422ecd8a65c279
pedraofiec <pedro41691@fiec.edu.br>
2025-03-28 22:28:17 -0300

    Update tabela.sql

--- tabela.sql
+++ tabela.sql
@@ -1,4 +1,10 @@
-
+CREATE TABLE Autores (
+    id_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,           
+    nacionalidade VARCHAR(100) NOT NULL 
+);
 
 CREATE TABLE emprestimos (
     id_emprestimo INT PRIMARY KEY IDENTITY(1,1),
